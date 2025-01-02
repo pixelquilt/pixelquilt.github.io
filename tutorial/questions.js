e2f6519d639f5669170b53774019734feedee66b
pixelquilt <cornell.brian.c@gmail.com>
2025-01-02 21:16:07 +0000

    Add first section to tutorial 4

---
+++ tutorial/questions.js
@@ -0,0 +1,25 @@
+class Question {
+    eAnswer = null;
+    visible = false;
+    constructor(elem) {
+        let answerText = elem.getAttribute("answer");
+        this.eAnswer = document.createElement("answer");
+        this.eAnswer.innerText = "Answer: " + answerText;
+        elem.insertAdjacentElement(this.eAnswer);
+        elem.addEventListener("click", this.toggle.bind(this));
+    }
+
+    toggle() {
+        if (this.visible) {
+            this.eAnswer.style.display = "none";
+            this.visible = false;
+        } else {
+            this.eAnswer.style.display = "block";
+            this.visible = true;
+        }
+    }
+}
+
+document.querySelectorAll("question").forEach(elem => {
+    new Question(elem);
+});
